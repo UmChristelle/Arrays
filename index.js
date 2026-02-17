b11910cf137e5b2c5b97dd4035b6325a711b76a7
Umchristelle <umutonichristella17@gmail.com>
2026-02-17 14:41:50 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+//Exercises 1:Array initialisation and property Acess
+const productCategories = ["Electronics", "Books", "Clothing", "Food", "Toys"];
+//a
+console.log(productCategories[0]);
+//b
+console.log(productCategories[productCategories.length - 1]);
+//c
+console.log(productCategories.length);
+//d
+productCategories[1] = "Stationery";
+console.log(productCategories);
